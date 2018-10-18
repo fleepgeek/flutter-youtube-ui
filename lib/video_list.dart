b3f8dcc30f505e5f60b11cea0e4dd8840a5eab5e
emmanuelokiche <fleepgeek@gmail.com>
2018-10-18 17:26:11 +0100

    made the video list rsponsive

--- lib/video_list.dart
+++ lib/video_list.dart
@@ -7,42 +7,100 @@ class VideoList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final deviceOrientation = MediaQuery.of(context).orientation;
     return ListView.separated(
-        itemBuilder: (context, index) {
-          return Column(
-            children: <Widget>[
-              Container(
-                width: MediaQuery.of(context).size.width,
-                height: 200.0,
-                decoration: BoxDecoration(
-                  image: DecorationImage(
-                      image: NetworkImage(listData[index].thumbNail),
-                      fit: BoxFit.cover),
-                ),
-              ),
-              ListTile(
-                contentPadding: const EdgeInsets.all(8.0),
-                dense: true,
-                leading: CircleAvatar(
-                  backgroundImage: NetworkImage(listData[index].channelAvatar),
-                ),
-                title: Padding(
-                  padding: const EdgeInsets.only(bottom: 4.0),
-                  child: Text(listData[index].title),
-                ),
-                subtitle: Text(
-                    "${listData[index].channelTitle} . ${listData[index].viewCount} . ${listData[index].publishedTime}"),
-                trailing: Container(
-                    margin: const EdgeInsets.only(bottom: 20.0),
-                    child: Icon(Icons.more_vert)),
-              ),
-            ],
-          );
-        },
-        separatorBuilder: (context, index) => Divider(
-              height: 1.0,
-              color: Colors.grey,
-            ),
-        itemCount: listData.length);
+      itemBuilder: (context, index) {
+        if(deviceOrientation == Orientation.portrait) {
+          return _buildPortraitList(context, index);
+        }else {
+          return _buildLandscapeList(context, index);
+        }
+      },
+      separatorBuilder: (context, index) => Divider(
+            height: 1.0,
+            color: Colors.grey,
+          ),
+      itemCount: listData.length,
+    );
   }
+
+  Widget _buildPortraitList(BuildContext context, int index) {
+    return Column(
+      children: <Widget>[
+        Container(
+          width: MediaQuery.of(context).size.width,
+          height: 200.0,
+          decoration: BoxDecoration(
+            image: DecorationImage(
+                image: NetworkImage(listData[index].thumbNail),
+                fit: BoxFit.cover),
+          ),
+        ),
+        ListTile(
+          contentPadding: const EdgeInsets.all(8.0),
+          dense: true,
+          leading: CircleAvatar(
+            backgroundImage: NetworkImage(listData[index].channelAvatar),
+          ),
+          title: Padding(
+            padding: const EdgeInsets.only(bottom: 4.0),
+            child: Text(listData[index].title),
+          ),
+          subtitle: Text(
+              "${listData[index].channelTitle} . ${listData[index].viewCount} . ${listData[index].publishedTime}"),
+          trailing: Container(
+              margin: const EdgeInsets.only(bottom: 20.0),
+              child: Icon(Icons.more_vert)),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildLandscapeList(BuildContext context, int index) {
+    return Container(
+      padding: const EdgeInsets.all(12.0),
+      child: Row(
+        children: <Widget>[
+          Container(
+//          width: MediaQuery.of(context).size.width / 2,
+          width: 336.0/1.5,
+            height: 188.0/1.5,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                  image: NetworkImage(listData[index].thumbNail),
+                  fit: BoxFit.cover),
+            ),
+          ),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                ListTile(
+                  contentPadding: const EdgeInsets.all(8.0),
+//                  dense: true,
+                  title: Padding(
+                    padding: const EdgeInsets.only(bottom: 4.0),
+                    child: Text(listData[index].title),
+                  ),
+                  subtitle: Text(
+                      "${listData[index].channelTitle} . ${listData[index].viewCount} . ${listData[index].publishedTime}"),
+                  trailing: Container(
+                      margin: const EdgeInsets.only(bottom: 30.0),
+                      child: Icon(Icons.more_vert)),
+                ),
+                Container(
+                  padding: const EdgeInsets.only(left: 8.0),
+                  child: CircleAvatar(
+                    backgroundImage: NetworkImage(listData[index].channelAvatar),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+
 }
